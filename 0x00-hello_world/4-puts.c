5c110dbe70725ad751e8fc1416dc1c761b303462
Mounib13 <omar.mounib8899@gmail.com>
2022-09-08 13:22:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  *
- ** Description: main - Prints out a string to stdout.
+ * main - Entry point
  *
- ** Return: 0 if success.
+ ** Return: Always 0 if (Success)
  *
  **/
 int main(void)
@@ -11,4 +11,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
